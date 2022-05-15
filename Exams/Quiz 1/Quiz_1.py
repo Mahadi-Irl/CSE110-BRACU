ee43780e32016df26ee60aefaaf1fcb9800f8366
Mahadi Ibne Bakar <105577484+Mahadi-Irl@users.noreply.github.com>
2022-05-15 20:12:05 +0600

    Create Quiz_1.py

---
+++ Exams/Quiz 1/Quiz_1.py
@@ -0,0 +1,24 @@
+# start
+
+# take input from user
+
+num = int(input('Please enter a number from 10 to 20 : '))
+
+# set conditions
+7
+y = num
+
+if num >= 10 and num <= 20 :
+    print ('The user entered',num)
+    print (num ,end='')
+    for i in range(99999)  :
+        x = 2 * y -5
+        if x >=100 :
+            break
+        print (',',x,end='')
+        y = x
+        
+else :
+    print ('Wrong Input')
+
+# end
